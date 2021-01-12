f1764f4cdb8187061cc5e064a2feec962f13a535
Jeremy H <jeremyhyf@gmail.com>
2021-01-12 23:56:19 +0800

    JHU Data Viz with R Course 1 Completed

---
+++ Course 1 Getting Started/1 Intro Basics/week-1-peer-review.R
@@ -0,0 +1,33 @@
+# Week 1 Practice Code
+
+# Type your name here: Jeremy
+
+# Complete these coding tasks, then submit your R file for another student to compare against the example output.
+
+# Problem 1 
+
+# Create a data frame that includes two columns, one named "Animals" and the other named "Foods". The first column should be this vector (note the intentional repeated values): Dog, Cat, Fish, Fish, Lizard
+
+#The second column should be this vector: Bread, Orange, Chocolate, Carrots, Milk
+
+#### Write your code below:
+Animals <- c("Dog", "Cat", "Fish", "Fish", "Lizard")
+Foods <- c("Bread", "Orange", "Chocolate", "Carrots", "Milk")
+dat <- data.frame(Animals, Foods)
+
+dat
+
+# Problem 2
+
+# Using the data frame created in Problem 2, use the table() command to create a frequency table for the column called "Animals".
+
+#### Write your code below:
+table(dat$Animals)
+
+# Problem 3
+
+# Use read.csv() to import the survey data included in this assignment. Using that data, make a histogram of the column called "pid7".
+
+#### Write your code below:
+cces <- read.csv('cces_sample_coursera.csv')
+hist(cces$pid7)
